cd246d2b8a85945e11851711b04e41ae9d7bb9d7
Roop <roopkumar@me.com>
2016-11-21 02:41:53 -0500

    jquery in youtube

--- public/project/views/home2/home2.controller.client.js
+++ public/project/views/home2/home2.controller.client.js
@@ -15,6 +15,11 @@
             YouTubeService.searchResult(searchText)
                 .success(function (response){
                     vm.youtubeResults = response.items;
+                    vm.song = "https://www.youtube.com/watch?v=fuYR5rPADrA";
+                    vm.songsObj = {'skin':'skins/tunes/skin.css','volume':50,'autoplay':false,
+                        'shuffle':false,'repeat':1,'placement':'bottom','showplaylist':true,
+                        'playlist':[{'title':'','url':'{{model.song}}'},
+                            {'title':'','url':'https://www.youtube.com/watch?v=uuK2BnzKGNU'}]};
                 })
                 .error(function (error) {
                     console.log(error);
