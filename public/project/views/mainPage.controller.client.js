e5328f233b159aeb345cfa8607ecd2154502af07
amardeep0070 <amardeep007-@gmail.com>
2016-11-28 19:44:10 -0500

    UserService

--- public/project/views/mainPage.controller.client.js
+++ public/project/views/mainPage.controller.client.js
@@ -9,12 +9,29 @@
         .module("MusicUnity")
         .controller("MainPageController",MainPageController )
 
-    function MainPageController($location) {
+    function MainPageController($location,UserService) {
         var vm=this;
         vm.signup=signup;
+        vm.login=login;
         function signup () {
             $location.url("/signup");
         }
 
+        function login (user) {
+            UserService.findUserByCredentials(user.username,user.password)
+                .success(function (user) {
+                    if(user!='0'){
+                        $location.url("/profile");
+                    }
+                    else{
+                        vm.error="User Not found";
+                    }
+                })
+                .error(function (error) {
+                    console.log("error in login controller")
+                })
+
+
+        }
     }
 })();
