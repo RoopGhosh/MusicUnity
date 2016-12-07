1a6bec2bed47edec77d42e24658d20aaa96e33af
amardeep0070 <amardeep007-@gmail.com>
2016-12-06 19:33:28 -0500

    Follow UI for Home 1

--- public/project/views/home1/home1.controller.client.js
+++ public/project/views/home1/home1.controller.client.js
@@ -6,8 +6,24 @@
     function Home1Controller($routeParams,UserService,LikeService,$http,$location,CommentService) {
         var vm = this;
         vm.userid = $routeParams['uid'];
+        vm.users=[{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},{username:"amardeep0070"},];
         vm.FollowerComments=[];
         vm.search = search;
+        vm.followUser=followUser;
+        vm.unfollowUser=unfollowUser;
+        function followUser(user,index) {
+            var follow='#follow'+ index;
+            $(follow).attr('class','hide');
+            var unfollow='#unfollow'+index
+            $(unfollow).attr('class','show')
+        }
+
+        function unfollowUser (user,index) {
+            var follow='#follow'+ index;
+            $(follow).attr('class','show');
+            var unfollow='#unfollow'+index
+            $(unfollow).attr('class','hide')
+        }
         function init() {
             UserService.findUserById(vm.userid)
                 .success(function (user) {
